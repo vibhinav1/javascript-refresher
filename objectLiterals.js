53fea5379d65934ab3d8c50147323350df77c986
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 15:16:43 -0500

    Create objectLiterals.js

---
+++ objectLiterals.js
@@ -0,0 +1,9 @@
+// object literals
+let user = {
+  name: 'crystal',
+  age: 30,
+  email: 'crystal@thenetninja.co.uk',
+  location: 'berlin',
+  blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
+console.log(user);||
