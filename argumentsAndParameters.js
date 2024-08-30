72c5651b20de8c3951a4ca0e0ab491a5e13fa338
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 13:14:47 -0500

    Update argumentsAndParameters.js

--- argumentsAndParameters.js
+++ argumentsAndParameters.js
@@ -3,3 +3,13 @@ const speak = function(name, time){
   console.log( good ${time} ${name}`);
 };
 speak('mario', 'morning');
+
+
+
+//
+// arguments & parameters
+const speak = function(name 'luigi', time 'night'){
+  console.log(good ${time} ${name}`);
+};
+speak();
+speak('shaun', 'day');   // these values are gonna override 
