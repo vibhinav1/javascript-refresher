96d0df0241793e31ed88c15eaf8ef66306529924
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 10:41:59 -0500

    Create ConditionalStatements.js

---
+++ ConditionalStatements.js
@@ -0,0 +1,9 @@
+// if statements
+const age = 25;
+if(age > 20){
+console.log('you are over 20 years old');
+}
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+if (ninjas.length > 4){
+console.log("that's a lot of ninjas");
+}
