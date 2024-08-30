09286874a70fe219e81ce3236b2b17f8debeea39
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 13:50:28 -0500

    Update forLoops.js

--- forLoops.js
+++ forLoops.js
@@ -9,3 +9,15 @@ for(let i = 0; i < names.length; i++){
   let html = `<div>${names[i]}</div>`;
   console.log(html);
 }
+
+
+//
+const bill = function (products, tax){
+  let total 0;
+            -
+  for(let i = 0; i < products.length; i++){
+! total += products[i] + products[i] * tax;
+  }
+  return total;
+}
+console.log(bill([10,15,30], 0.2));
