3f8c433d4bb9895445ab7a979679feda0256278c
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 09:05:58 -0500

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+// ninjas [1] = 'ken';
+// console.log(ninjas[1]);
+// let ages = [20,25,30,35];
+//console.log(ages [2]);
+// let random ['shaun', 'crystal', 30, 20
+// console.log(random);
+//console.log(ninjas.length);
+// array methods
+let result = ninjas.join('-');
+console.log(result);
