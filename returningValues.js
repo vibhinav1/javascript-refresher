cc915224792cda3bf19fd339b105aa27b0ce6953
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 13:18:26 -0500

    Create returningValues.js

---
+++ returningValues.js
@@ -0,0 +1,10 @@
+// returning values
+// const speak = function(name 'luigi', time = 'night'){
+// console.log( good ${time} ${name}`);
+// };
+const calcArea = function(radius){
+  let area = 3.14 * radius**2;
+  return area;
+}
+const area = calcArea(5);
+console.log(area);
