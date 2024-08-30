41b044a960839ebfd3999a59df62b9c289bfbc07
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 15:21:05 -0500

    Update objectLiterals.js

--- objectLiterals.js
+++ objectLiterals.js
@@ -21,3 +21,7 @@ console.log(user);
 console.log(user.name);
 //user.age = 35;
 console.log(user.age);
+console.log(user['location']);
+user['name'] = 'chun-li';
+console.log(user['name']);
+console.log(typeof user);
