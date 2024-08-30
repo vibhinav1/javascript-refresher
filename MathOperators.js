cef1b7b0ac3dcefeea5eaa3d02ab8a2a45ecc04a
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 09:02:18 -0500

    Create MathOperators.js

---
+++ MathOperators.js
@@ -0,0 +1,14 @@
+let radius = 10;
+const pi = 3.14;
+//console.log(radius, pi)%;B
+//math operators +, * /, **
+, ر
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi* radius**2;
+// order of operation - BIDMAS
+//let result = 5* (10-3)**2;
+//console.log(result);
+let likes = 10%;
+likes = likes + 1;
+console.log(likes);
