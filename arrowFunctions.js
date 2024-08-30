2ba59ac9bf43b8ab4ad6b7f07cc6b80d67ec897b
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 14:35:44 -0500

    Update arrowFunctions.js

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -13,3 +13,23 @@ const bill = function (products, tax){
   }
   return total;
 }
+
+//
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
+// const bill = function (products, tax) {
+// let total = 0;
+// for(let i = 0; i < products.length; i++){
+// total += products[i] + products[i] * tax;
+// }
+// return total;
+// }
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+};
+console.log(bill([10,15,30], 0.2));
