8d24fd1d973357ee3481b9c851ac843382cd740e
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 09:01:20 -0500

    Create stringMethods.js

---
+++ stringMethods.js
@@ -0,0 +1,8 @@
+// common string methods
+let email = 'mario@thenetninja.co.uk
+// let result = email.lastIndexOf
+// let result = email.slice
+//let result = email.substr
+//let result = email.replace
+let result = email.replace(
+console.log(result);
