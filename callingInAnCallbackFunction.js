80c07cd41be84c7b9b82f26d1759519365cd3d5b
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 15:10:45 -0500

    Update callingInAnCallbackFunction.js

--- callingInAnCallbackFunction.js
+++ callingInAnCallbackFunction.js
@@ -5,3 +5,18 @@ people.forEach(function(person){
   html += `<li style="color: purple">${person}</li>`;
 });
 console.log(html);
+
+
+//
+
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+let html =
+!
+people.forEach(function(person) {
+  // create html template
+  html += `<li style="color: purple">${person}</li>`;
+});
+console.log(html);
+ul.innerHTML = html;
