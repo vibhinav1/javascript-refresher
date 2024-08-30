aaa478b7843751d5ea8218a4b3f45ea3928af6e9
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 14:49:49 -0500

    Update callbackFunctionsAndForeach.js

--- callbackFunctionsAndForeach.js
+++ callbackFunctionsAndForeach.js
@@ -3,3 +3,11 @@ let people ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 people.forEach(function(){
   console.log('something');
 });
+
+//
+// callbacks & foreach in array calling callback function
+!
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+people.forEach(function(person) {
+  console.log('something');
+});
