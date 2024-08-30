ef01d8a240f34c6babec060273367ebdf6f96e22
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 11:19:37 -0500

    Update logicalOperators.js

--- logicalOperators.js
+++ logicalOperators.js
@@ -8,3 +8,14 @@ console. log('that password is long enough! ');
 console. log('password is not long enough');
 }
 
+// logical operators OR and AND &&
+const password = 'password';
+if(password.length >= 12 && password. includes('@')){
+console.log('that password is mighty strong');
+}
+else if(password.length >= 8 || password.includes('@') && password.length > 5)
+{
+console.log('that password is strong enough!');
+}
+else{
+console.log('password is not strong enough');}
