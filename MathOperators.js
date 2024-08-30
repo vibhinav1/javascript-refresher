56ace1154e6bb7c107e346672fad38499a116f55
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 09:03:41 -0500

    Update MathOperators.js

--- MathOperators.js
+++ MathOperators.js
@@ -9,6 +9,21 @@ const pi = 3.14;
 // order of operation - BIDMAS
 //let result = 5* (10-3)**2;
 //console.log(result);
-let likes = 10%;
+let likes = 10;
 likes = likes + 1;
 console.log(likes);
+
+//let result = 5* (10-3)**2;
+//console.log(result);
+let likes = 10;
+//likes = likes + 1;
+//likes++;
+//likes--;
+//likes += 10;
+//likes == 5;
+//likes *= 2;
+//likes /= 2;
+//console.log(likes);
+// NaN not a number
+console.log(5 / 'hello');
+console.log(5 * 'hello');
