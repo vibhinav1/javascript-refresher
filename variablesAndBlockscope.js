db606aef68668df487b3f1f4f1388ca6212b4a20
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 11:39:55 -0500

    Create variablesAndBlockscope.js

---
+++ variablesAndBlockscope.js
@@ -0,0 +1,8 @@
+let age = 30;
+if (true)
+    {
+   let age = 40;          //defined the age here .(if we used age = 40 only, the result of inside block will be  40 as above )
+  console.log('inside 1st code block:', age);
+    }
+
+    console.log('outside code block:', age);
