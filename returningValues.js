95893c82ca7f12642abf31d5750398202b28842d
vibhinav1 <164300476+vibhinav1@users.noreply.github.com>
2024-08-30 13:19:52 -0500

    Update returningValues.js

--- returningValues.js
+++ returningValues.js
@@ -3,8 +3,9 @@
 // console.log( good ${time} ${name}`);
 // };
 const calcArea = function(radius){
-  let area = 3.14 * radius**2;
-  return area;
-}
-const area = calcArea(5);
-console.log(area);
+    let area = 3.14 * radius**2;
+    return area;
+  }
+  const answer = calcArea(5);
+  console.log(answer);
+  
